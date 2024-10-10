af3d871648a9637ea259c905e54de1446799ae7b
Ethan Wilde <ewilde@santarosa.edu>
2024-10-10 12:56:37 -0700

    .next removed

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components/Restaurant.jsx -> @/src/components/ReviewDialog.jsx\":{\"id\":\"components/Restaurant.jsx -> @/src/components/ReviewDialog.jsx\",\"files\":[\"static/chunks/_app-pages-browser_src_components_ReviewDialog_jsx.js\"]}}"

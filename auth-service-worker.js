e0737ea8ba6ad8315238bc99f10af51b1ea9067c
Ethan Wilde <ewilde@santarosa.edu>
2024-10-21 18:13:48 -0700

    github issue 307 fix?

--- auth-service-worker.js
+++ auth-service-worker.js
@@ -1,52 +1,34 @@
+
+// auth-service-worker.js
+
 import { initializeApp } from "firebase/app";
-import { getAuth, getIdToken } from "firebase/auth";
-import { getInstallations, getToken } from "firebase/installations";
+import { getAuth, getIdToken, onAuthStateChanged } from "firebase/auth";
 
-// this is set during install
-let firebaseConfig;
+// Extract Firebase config from query string
+const serializedFirebaseConfig = new URLSearchParams(self.location.search).get(
+  "firebaseConfig"
+);
+if (!serializedFirebaseConfig) {
+  throw new Error(
+    "Firebase Config object not found in service worker query string."
+  );
+}
 
-self.addEventListener('install', event => {
-  // extract firebase config from query string
-  const serializedFirebaseConfig = new URL(location).searchParams.get('firebaseConfig');
-  
-  if (!serializedFirebaseConfig) {
-    throw new Error('Firebase Config object not found in service worker query string.');
-  }
-  
-  firebaseConfig = JSON.parse(serializedFirebaseConfig);
+const firebaseConfig = JSON.parse(serializedFirebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+self.addEventListener("install", () => {
   console.log("Service worker installed with Firebase config", firebaseConfig);
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
-  /* 
-  
-  FIX FROM https://github.com/firebase/friendlyeats-web/issues/295#issuecomment-2318574547
-  
-    firebase env vars not saved in firebaseConfig
-
-  FIX START 
-  */
-  if (!firebaseConfig) {
-    const serializedFirebaseConfig = new URL(location).searchParams.get(
-      "firebaseConfig"
-    );
-    firebaseConfig = JSON.parse(serializedFirebaseConfig);
-  }
-  /* FIX END */
-  /* 
-  
-  FIX FROM https://github.com/firebase/friendlyeats-web/pull/307#issuecomment-2359673660
-  
-    make sure currentUser is not null (part 1 of 2)
-
-  FIX START 
-  */
- /*
-  const { origin } = new URL(event.request.url);
-  if (origin !== self.location.origin) return;
-  event.respondWith(fetchWithFirebaseHeaders(event.request));
-  */
-
   const { origin, pathname } = new URL(event.request.url);
   if (origin !== self.location.origin) return;
 
@@ -60,58 +42,71 @@ self.addEventListener("fetch", (event) => {
 
   if (pathname.startsWith("/_next/")) return;
 
-  // Don't add headers to non-GET/POST requests or those with an extension
+  // Don't add headers to non-GET requests or those with an extension
   // This helps with CSS, images, fonts, JSON, etc.
   if ((event.request.method === "GET" || event.request.method === "POST") && !pathname.includes(".")) {
     event.respondWith(fetchWithFirebaseHeaders(event.request));
   }
-  /* FIX END */
 });
 
 async function fetchWithFirebaseHeaders(request) {
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const installations = getInstallations(app);
-  const headers = new Headers(request.headers);
-  /* 
-  
-  FIX FROM https://github.com/firebase/friendlyeats-web/pull/307#issuecomment-2395066549
-  
-    make sure currentUser is not null (part 2 of 2)
-
-  FIX START 
-  */
- /*
-  const [authIdToken, installationToken] = await Promise.all([
-    getAuthIdToken(auth),
-    getToken(installations),
-  ]);
-  */
-  let [authIdToken, installationToken] = await Promise.all([
-    getAuthIdToken(auth),
-    getToken(installations),
-  ]);
+  let authIdToken = await getAuthIdToken();
   if (!authIdToken) {
-    console.log("authIdToken null once");
     // sleep for 0.25s
     await new Promise((resolve) => setTimeout(resolve, 250));
     authIdToken = await getAuthIdToken();
   }
   if (!authIdToken) {
-    console.log("authIdToken null twice");
     // sleep for 0.25s
     await new Promise((resolve) => setTimeout(resolve, 250));
     authIdToken = await getAuthIdToken();
   }
-  /* FIX END */
-  headers.append("Firebase-Instance-ID-Token", installationToken);
-  if (authIdToken) headers.append("Authorization", `Bearer ${authIdToken}`);
-  const newRequest = new Request(request, { headers });
-  return await fetch(newRequest);
+  if (authIdToken) {
+    const headers = new Headers(request.headers);
+    headers.append("Authorization", `Bearer ${authIdToken}`);
+    request = new Request(request, { headers });
+  }
+  return await fetch(request).catch((reason) => {
+    console.error(reason);
+    return new Response("Fail.", {
+      status: 500,
+      headers: { "Content-Type": "text/html" },
+    });
+  });
 }
 
-async function getAuthIdToken(auth) {
-  await auth.authStateReady();
-  if (!auth.currentUser) return;
+async function waitForMatchingUid(_uid) {
+  const uid = _uid === "undefined" ? undefined : _uid;
+  await authStateReady();
+  await new Promise((resolve) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user?.uid === uid) {
+        unsubscribe();
+        resolve();
+      }
+    });
+  });
+  return new Response(undefined, {
+    status: 200,
+    headers: { "Cache-Control": "no-store" },
+  });
+}
+
+function authStateReady() {
+  return new Promise((resolve) => {
+    if (auth.currentUser !== undefined) {
+      resolve();
+    } else {
+      const unsubscribe = onAuthStateChanged(auth, () => {
+        unsubscribe();
+        resolve();
+      });
+    }
+  });
+}
+
+async function getAuthIdToken() {
+  await authStateReady();
+  if (!auth.currentUser) return null;
   return await getIdToken(auth.currentUser);
 }
